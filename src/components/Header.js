550d15e4c1d4326e116f80281bbba0a54bddb677
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-31 12:54:26 -0500

    styled welcome page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,37 @@ import React from "react";
 import { Link, Route} from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 import CharacterList from './CharacterList';
+import styled from 'styled-components';
+
+const Nav = styled.nav `
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+  background: #3d85c6;
+  margin: -1rem -1rem 2.5rem -1rem;
+  padding: 1.5rem;
+`;
+
+const Links = styled(Link) `
+  color: black;
+  background: white;
+  padding: .5rem 1.5rem; 
+  font-size: 2rem;
+  border: 1px solid black;
+  border-radius: 10px;
+  &:hover {
+    color: tomato;
+    text-decoration: none;
+  }
+`;
 
 export default function Header() {
   return (
     <header className="ui centered">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/character_list">Character's</Link>
-      </nav>
+      <Nav>
+        <Links to="/">Home</Links>
+        <Links to="/character_list">Character List</Links>
+      </Nav>
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
       <Route exact path="/" component={WelcomePage} />
       <Route path="/character_list" component={CharacterList} />
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,5 +1,29 @@
 import React from "react";
 import { useHistory} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Button = styled.button `
+  background:    #3d85c6;
+  border:        1px solid #000000;
+  border-radius: 5px;
+  box-shadow:    1px 1px #000000;
+  padding:       8px 20px;
+  color:         #ffffff;
+  display:       inline-block;
+  font:          normal bold 24px/1 "Calibri", sans-serif;
+  text-align:    center;
+  text-shadow:   1px 1px #000000;
+  margin: 2rem 25.5rem;
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    color: #3d85c6;
+    background: #ffffff;
+    border: 1px solid #3d85c6;
+  }
+`;
+
 
 export default function WelcomePage() {
   const history = useHistory();
@@ -17,7 +41,7 @@ export default function WelcomePage() {
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-        <button onClick={routeToCharacter}>Character's</button>
+        <Button onClick={routeToCharacter}>Character's</Button>
       </header>
     </section>
   );
