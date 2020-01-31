def1bf161c44ce0163d16c28ff33d5287ea6bc36
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-31 11:46:45 -0500

    added Routing to Welcome page and Character List

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then(res => {
+      console.log('This is my characters array: ',res.data.results)
+      setCharacter(res.data.results);
+    })
+    .catch(err => {
+      console.error("You are getting an error of: ", err); 
+    })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Character's</h2>
+      
     </section>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link, Route} from 'react-router-dom';
+import WelcomePage from './WelcomePage';
+import CharacterList from './CharacterList';
 
 export default function Header() {
   return (
     <header className="ui centered">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/character_list">Character's</Link>
+      </nav>
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/character_list" component={CharacterList} />
     </header>
   );
 }
